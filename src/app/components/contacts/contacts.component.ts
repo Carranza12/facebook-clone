f6c24a2a0bee91c82c098adbe9345bf0f20b5d09
Francisco Carranza <Carranza12>
2023-02-01 13:43:30 -0600

    cambios

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent implements OnInit {
-
+  users = [1,2,3,4,5,6,7,8,9,10];
   constructor() { }
 
   ngOnInit(): void {
